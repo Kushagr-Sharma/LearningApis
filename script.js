1a513469e0a62f7f3987ac9313cf044f3f9660b7
Kushagr-Sharma <59696446+Kushagr-Sharma@users.noreply.github.com>
2020-03-02 01:57:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ var formdata = new FormData();
 var requestOptions = {
   redirect: 'follow'
 };
-function appendData(data) {
-    var mainContainer = document.getElementById("missions");
-    for (var i = 0; i < data.length; i++) {
-        var div = document.createElement('div');
-        div.innerHTML = 'mission_name: ' + data[i].mission_name+ ' mission_id:' + data[i].mission_id+ ' description:'+ data[i].description ;
-        mainContainer.appendChild(div);
-      }
-    }
-
+function appendMain(data)
+{
+  var mainContainer =document.getElementById("mission");
+  for(var i = 0; i < data.length;i++)
+  {
+    var div = document.createElement('div') ;
+    div.innerHTML +='<li class ="other"><b><i><u>Mission_name : </u></i></b><br/>'+ data[i].mission_name+'<br/></li>';
+    mainContainer.appendChild(div);
+    mainContainer =document.getElementById("mission");
+    div.innerHTML += '<li class ="other"><b><i><u>Mission_id : </u></i></b><br/>'+ data[i].mission_id+'<br/></li>';
+    mainContainer.appendChild(div);
+    mainContainer =document.getElementById("mission");
+    div.innerHTML +='<li class ="desc"><b><i><u>Description : </u></i></b><br/>'+ data[i].description+'</li>';
+    mainContainer.appendChild(div);
+  }
+}
 fetch("https://api.spacexdata.com/v3/missions/?limit=10", requestOptions)
 .then(function (response) {
     return response.json();
 })
 .then(function (data) {
-    appendData(data);
+    appendMain(data);
 })
 .catch(function (err) {
     console.log('error: ' + err);
